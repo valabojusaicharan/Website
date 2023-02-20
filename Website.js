b2c83a43f2ae39d044fff0d50b745778ed50a36a
voldemort1234567890 <80281483+voldemort1234567890@users.noreply.github.com>
2023-02-20 08:44:33 +0530

    Add files via upload

---
+++ Website.js
@@ -0,0 +1,50 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+   slides[slideIndex-1].style.display = "block";
+}
+
+var accc = document.getElementsByClassName("accordions");
+var i;
+
+for (i = 0; i < accc.length; i++) {
+  accc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function initMap() {
+  const uluru = { lat: 17.172283, lng: 131.031 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
